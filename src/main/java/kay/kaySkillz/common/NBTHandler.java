d16d3431304f91562b6c1c75d4c0fa53ceda934c
DrZed <kyahne12@yahoo.com>
2014-08-09 07:27:37 -0500

    Changing Shizzle
    Going to sleep continuing work tomarrow

---
+++ src/main/java/kay/kaySkillz/common/NBTHandler.java
@@ -0,0 +1,32 @@
+package kay.kaySkillz.common;
+
+import cpw.mods.fml.common.Mod;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
+public class NBTHandler{
+
+    public static int SkillAgilityXP = 0;
+    public static int SkillAgilityLVL = 0;
+
+    public static int SkillImmuneXP = 0;
+    public static int SkillImmuneLVL = 0;
+
+    @Mod.EventHandler
+    public void NBTHandlerCore(IExtendedEntityProperties tag, int data){
+
+    }
+
+
+    public void ModifyLVLStats(){
+        int LevelReqAg = Config.Difficulty * (SkillAgilityLVL + 1);
+        int LevelReqIm = Config.Difficulty * (SkillImmuneLVL + 1);
+        if (SkillAgilityXP >= LevelReqAg){
+            SkillAgilityXP = SkillAgilityXP - LevelReqAg;
+            SkillAgilityLVL = SkillAgilityLVL + 1;
+        }
+        if (SkillImmuneXP >= LevelReqIm){
+            SkillImmuneXP = SkillImmuneXP - LevelReqIm;
+            SkillImmuneLVL = SkillImmuneLVL + 1;
+        }
+    }
+}
